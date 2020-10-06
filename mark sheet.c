5ca5f1ec91d34b3a1bee544ece68684ad7252617
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-10-06 12:05:16 +0500

    updated marksheet

--- mark sheet.c
+++ mark sheet.c
@@ -1,48 +1,71 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-		char name [50], class [40]; int rollnum,mathematics,physics,chemistry,english; float total,percentage;
-	    printf("\tPLEASE ENTER YOUR NAME\n");
-	    scanf("\t%s",&name);
-	    printf("\tPLEASE ENTER YOUR class\n");
-	    scanf("\t%s",&class);
-	    printf("\tPLEASE ENTER YOUR ROLL NUMBER\n");
-	    scanf("\t%d",&rollnum);
-	printf("\tPLEASE ENTER YOUR marks of mathematics\n");
-	    scanf("\t%d",&mathematics);
-	printf("\tPLEASE ENTER YOUR marks of physics\n");
-	    scanf("\t%d",&physics);
-	printf("\tPLEASE ENTER YOUR marks of chemistry\n");
-	    scanf("\t%d",&chemistry);
-	printf("\tPLEASE ENTER YOUR marks of english\n");
-	    scanf("\t%d",&english);
-	total=mathematics+physics+chemistry+english;
-	percentage=total/400*100;
-	                                               
-	    printf("\tYOU HAVE ENTERED %s as your name \n",name);
-	    printf("\tYOU HAVE ENTERED %s as your class \n",class);                                           
-	    printf("\tYOU HAVE ENTERED %d as your roll number \n", rollnum);                                           		                                           
-	    printf("\tYOUR TOTAL NUMBERS ARE %f\n", total);
-	    printf("\tYOUR PERCENTAGE IS %f",percentage );
-	    printf("%%");
-	
-	
-	if(percentage>=80) {
-		printf("\t\nYOU GOT 'A+ IN YOUR EXAMS AND BEST OF LUCK FOR YOUR FUTURE");
-		}
-	else if(percentage>=70){
-		printf("\t\nYOU GOT 'A' IN YOUR EXAMS AND BEST OF LUCK FOR YOUR FUTURE");
-	}
-	else if(percentage>=60){
-		printf("\t\nYOU GOT 'B' IN YOUR EXAMS AND IMPROVE YOURSELF");
-	}
-	else if(percentage>=50){
-		printf("\t\nYOU GOT 'C' IN YOUR EXAMS AND WORK HARD FOR NEXT EXAM");
-	}
-	else if(percentage<50){
-		printf("\t\nYOU GOT 'F' IN YOUR EXAMS AND BRING YOUR PARENTS BY TOMORROW ");
-	}
-	
-	getch();
-
+#include<iostream.h>
+#include<conio.h>
+#include<iomanip.h>
+class marksheet
+{
+    public:
+    int rolln,cpp,java,n1,total;
+    float per;
+    char name[50];
+    void pn()
+    {
+        cout<<"\n Enter the roll number: ";
+        cin>>rolln;
+        cout<<"\n Enter the name of student: ";
+        cin>>name;
+        cout<<"\n Enter the marks of c plus plus: ";
+        cin>>cpp;
+        cout<<"\n Enter the marks of java: ";
+        cin>>java;
+        cout<<"\n Enter the marks of n1: ";
+        cin>>n1;
+    }
+    void pro()
+    {
+        total=cpp+java+n1;
+        per=total/3;
+    }
+    void gd()
+    {
+        if(per>=80)
+        {
+            cout<<"\n Grade=Distinction";
+        }
+        else if(per>=60 && per<80)
+        {
+            cout<<"\n Grade=A";
+        }
+        else if( per>=50 && per<60)
+        {
+            cout<<"\n Grade=B";
+        }
+        else if(per>=35 && per<50)
+        {
+            cout<<"\n Grade=C";
+        }
+        else if(per<35)
+        {
+            cout<<"\n Grade=Fail";
+        }
+    }
+    void gn()
+    {
+        cout<<"\n Roll Number== "<<setw(13)<<rolln;
+        cout<<"\n Name== "<<setw(20)<<name;
+        cout<<"\n Marks of CPlusPlus== "<<setw(6)<<cpp;
+        cout<<"\n Marks of Java== "<<setw(11)<<java;
+        cout<<"\n Marks of N1== "<<setw(13)<<n1;
+        cout<<"\n Total Marks== "<<setw(13)<<total;
+        cout<<"\n Percentage== "<<setw(14)<<per;
+    }
+};
+void main()
+{
+    clrscr();
+    marksheet m;
+    m.pn();
+    m.pro();
+    m.gn();
+    m.gd();
+    getch();
 }
